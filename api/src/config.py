60e24ce2d22d86f1bcacd8542e45fc7f90750e25
jonesywolf <4johnryanwolf@gmail.com>
2024-11-11 23:57:06 -0500

    Add MongoDB connection support and added mongodb client to app state and lifespan management

--- api/src/config.py
+++ api/src/config.py
@@ -3,4 +3,5 @@ import os
 NEO4J_URI = os.getenv("NEO4J_URI", "neo4j://localhost:7687")
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO") # or "DEBUG", "WARNING", "ERROR", "CRITICAL"
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")  # or "DEBUG", "WARNING", "ERROR", "CRITICAL"
--- api/src/shared/state.py
+++ api/src/shared/state.py
@@ -1,33 +1,44 @@
 import logging
 from contextlib import asynccontextmanager
 from neo4j import GraphDatabase
+from pymongo import MongoClient
 from src.extern.api_worker import BlockchainAPIWorker
-from src.config import NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD
+from src.config import MONGO_URI, NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD
 
 logger = logging.getLogger(__name__)
+
 
 @asynccontextmanager
 async def lifespan(app):
     """
     Allow access to the Neo4j database and the Blockchain.com API worker in the app state and manager
     their life cycles.
-    
+
     Parameters:
     - app: The FastAPI application instance
     """
-    neo4j_driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
+    neo4j_driver = GraphDatabase.driver(
+        NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD)
+    )
     neo4j_driver.verify_connectivity()
     logger.info(f"Connected to Neo4j at {NEO4J_URI} as {NEO4J_USERNAME}")
     app.state.neo4j_driver = neo4j_driver
-    
+
+    mongo_client = MongoClient(MONGO_URI)
+    logger.info(f"Connected to MongoDB at {MONGO_URI}")
+    app.state.mongo_client = mongo_client
+
     blockchain_api_worker = BlockchainAPIWorker()
     logger.info("Started API worker")
     app.state.api_worker = blockchain_api_worker
-    
+
     yield
-    
+
     neo4j_driver.close()
     logger.info("Disconnected from Neo4j")
-    
+
+    mongo_client.close()
+    logger.info("Disconnected from MongoDB")
+
     await blockchain_api_worker.close()
     logger.info("Stopped API worker")
